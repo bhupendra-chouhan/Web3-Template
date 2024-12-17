144c9cceade97f95c81cf4e5a6bd9ffdd4918fde
Bhupendra <iiitianbhupendra@gmail.com>
2024-12-17 14:58:52 +0530

    Patch-1: Base Web3-template is Done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import "./App.css";
+import Routing from "./components/Routing";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Routing/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
